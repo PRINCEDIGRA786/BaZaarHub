9743dc55b095122194240a1acbbc4ba3e54f1226
Prince <121872553+PRINCEDIGRA786@users.noreply.github.com>
2023-11-23 18:31:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import axios from 'axios';
-import ChatGPT from './ChatGPT';
-
+import Navbar from './Components/Navbar';
+import Watchlist from './Components/Watchlist';
+import Login from './Components/Login'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from './Components/Signup';
+import Home from './Components/Home';
+import About from './Components/About';
+import Cart from './Components/Cart';
+import Apii from './Components/Apii';
+import Page from './Components/Page';
+import Pay from './Components/Pay';
+import toget from './State/reducers/tosendid';
+import { useDispatch, useSelector } from 'react-redux';
+import Offerpage from './Components/Offerpage';
 function App() {
-  const [prompt, setprompt] = useState("");
-  const [response, setresponse] = useState("");
-  const handleSubmit = async (e) => {
-    // e.preventDefault();
-
-    // const response = await fetch('http://localhost:8080/chat', { prompt }, {
-    //   method: 'POST'  // Sending a POST request
-    // });
-    axios
-      .post("http://localhost:8080/chat", { prompt })
-      .then((res) => {
-        setresponse(res.data);
-      })
-      .catch((err) => {
-        console.error("The error is", err);
-      })
-  }
+  var page = useSelector(state => state.Page)
   return (
     <>
-      {/* <div>
-        <input
-          type='text'
-          value={prompt}
-          onChange={(e) => setprompt(e.target.value)}>
-        </input>
-        <button style={{ 'backgroundcolor': 'green' }}
-          onClick={() => {
-            handleSubmit();
-          }}>Submit</button>
-      </div>
-      <div>
-        <p>
-          {response}
-        </p>
-
-      </div> */}
-
-      <ChatGPT />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/login/home' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/pay' element={<Pay />} />
+          <Route path='/page' element={<Page page={page} />} />
+          <Route path='/watchlist' element={<Watchlist />} />
+          <Route path="/grocery" element={<Apii category={'groceries'} />}></Route>
+          <Route path="/Fragrances" element={<Apii category={'fragrances'} />}></Route>
+          <Route path="/electronics" element={<Apii category={'electronics'} />}></Route>
+          <Route path="/appliances" element={<Apii category={'appliances'} />}></Route>
+          <Route path="/mobiles" element={<Apii category={'mobiles'} />}></Route>
+          <Route path="/watches" element={<Apii category={'watches'} />}></Route>
+          <Route path='/fashion' element={<Apii category={'fashion'} />}></Route>
+          <Route path='/offers' element={<Offerpage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
 export default App;
+
